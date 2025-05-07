64ba6bf8fa7511f46fd5f7dcbdec56412c0ea6a7
Justin Cose <justin@unitedmarket.com>
2025-05-07 16:19:16 -0500

    xtra mobile support

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,7 @@ export default async function RootLayout({
       </head>
       <body
         className={cn(
-          'min-h-screen flex flex-col font-sans antialiased',
+          'min-h-screen flex flex-col font-sans antialiased overflow-x-hidden',
           fontSans.variable
         )}
       >
